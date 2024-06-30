267cd26a24499df250829f1fea4429cbc5ac29a0
Claudio Carigé <ccarige@gmail.com>
2024-06-30 17:49:03 -0300

    [ addition ] : Created LegalPersonalCustomerDTO classs

---
+++ mspersistence/src/main/java/br/com/copyimagem/mspersistence/core/dtos/LegalPersonalCustomerDTO.java
@@ -0,0 +1,57 @@
+package br.com.copyimagem.mspersistence.core.dtos;
+
+import br.com.copyimagem.mspersistence.core.domain.entities.Address;
+import br.com.copyimagem.mspersistence.core.domain.entities.CustomerContract;
+import br.com.copyimagem.mspersistence.core.domain.entities.MonthlyPayment;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Pattern;
+import org.hibernate.validator.constraints.br.CNPJ;
+
+import java.io.Serial;
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
+
+
+public class LegalPersonalCustomerDTO implements Serializable {
+
+
+    @Serial
+    private static final long serialVersionUID = 1L;
+
+    private Long id;
+
+    @NotNull( message = "Name is required" )
+    private String clientName;
+
+    @CNPJ( message = "CNPJ format is invalid" )
+    @NotNull( message = "CNPJ is required" )
+    private String cnpj;
+
+    @NotNull( message = "Email is required" )
+    @Pattern( regexp = "^[a-zA-Z0-9._+-]+@[a-z]+\\.[a-z]{2,}(?:\\.[a-z]{2,3})?$", message = "Email format is invalid" )
+    private String primaryEmail;
+
+    private List< String > emailList = new ArrayList<>();
+
+    @NotNull( message = "Phone number is required" )
+    private String phoneNumber;
+
+    @NotNull( message = "Whatsapp is required" )
+    private String whatsapp;
+
+    private String bankCode;
+
+    private Address address;
+
+    private String financialSituation;
+
+    private byte payDay;
+
+    private CustomerContract customerContract;
+
+    private List< MultiPrinterDTO > multiPrinterList = new ArrayList<>();
+
+    private List< MonthlyPayment > monthlyPaymentList = new ArrayList<>();
+
+}
