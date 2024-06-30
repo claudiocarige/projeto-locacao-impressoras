3f6dcb0f85f39cf00682858e8dfaf5c61846d594
Claudio Carigé <ccarige@gmail.com>
2024-06-30 15:05:34 -0300

    First commit

---
+++ mspersistence/src/main/java/br/com/copyimagem/mspersistence/MspersistenceApplication.java
@@ -0,0 +1,13 @@
+package br.com.copyimagem.mspersistence;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MspersistenceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MspersistenceApplication.class, args);
+	}
+
+}
