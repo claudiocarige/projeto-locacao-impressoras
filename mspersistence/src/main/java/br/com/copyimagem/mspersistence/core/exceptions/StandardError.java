7537afa8db3bf1b2f89a83b863fd9e7fe35d32ec
Claudio Carigé <ccarige@gmail.com>
2024-07-01 20:28:49 -0300

    [ addition ] : Created StandardError class

---
+++ mspersistence/src/main/java/br/com/copyimagem/mspersistence/core/exceptions/StandardError.java
@@ -0,0 +1,23 @@
+package br.com.copyimagem.mspersistence.core.exceptions;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class StandardError {
+
+    private Long timestamp;
+
+    private Integer status;
+
+    private String message;
+
+    private String path;
+
+}
