8154c27287ec055b5295df4d8f526905d20e07bf
Claudio Carigé <ccarige@gmail.com>
2024-07-02 19:15:27 -0300

    [ test ] : Created findNaturalPersonCustomerById test method

--- mspersistence/src/test/java/br/com/copyimagem/mspersistence/infra/controllers/NaturalPersonCustomerControllerTest.java
+++ mspersistence/src/test/java/br/com/copyimagem/mspersistence/infra/controllers/NaturalPersonCustomerControllerTest.java
@@ -10,7 +10,6 @@ import org.junit.jupiter.api.Test;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
-import org.modelmapper.ModelMapper;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.test.web.servlet.MockMvc;
@@ -19,7 +18,7 @@ import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
-import static org.mockito.Mockito.when;
+import static org.mockito.Mockito.*;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
@@ -62,6 +61,24 @@ class NaturalPersonCustomerControllerTest {
                 .andExpect(status().isOk())
                 .andExpect(content().contentType(MediaType.APPLICATION_JSON))
                 .andExpect(jsonPath("$[0].id").value(1));
+    }
+
+    @Test
+    @DisplayName("Should return a NaturalPersonCustomer by id")
+    void shouldReturnANaturalPersonCustomerById() throws Exception {
+
+        when(naturalPersonCustomerService.findNaturalPersonCustomerById(customerPf.getId()))
+                .thenReturn(customerPfDTO);
+
+        mockMvc.perform(get("/api/v1/customers/pf/{id}", customerPfDTO.getId())
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .accept(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andExpect(content().contentType(MediaType.APPLICATION_JSON))
+                .andExpect(jsonPath("$.id").value(customerPfDTO.getId()))
+                .andExpect(jsonPath("$.clientName").value(customerPfDTO.getClientName())); // Add assertions for other fields
+        verify(naturalPersonCustomerService, times(1))
+                .findNaturalPersonCustomerById(customerPf.getId());
     }
 
     private void start() {
