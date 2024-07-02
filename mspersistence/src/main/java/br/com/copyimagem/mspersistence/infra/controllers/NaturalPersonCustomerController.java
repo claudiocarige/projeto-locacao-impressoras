890a94d802f53216333e60e662b2ee676bead50e
Claudio Carigé <ccarige@gmail.com>
2024-07-02 18:45:47 -0300

    [ addition ] : Created getAllNaturalPersonCustomers method

--- mspersistence/src/main/java/br/com/copyimagem/mspersistence/infra/controllers/NaturalPersonCustomerController.java
+++ mspersistence/src/main/java/br/com/copyimagem/mspersistence/infra/controllers/NaturalPersonCustomerController.java
@@ -1,9 +1,14 @@
 package br.com.copyimagem.mspersistence.infra.controllers;
 
+import br.com.copyimagem.mspersistence.core.dtos.NaturalPersonCustomerDTO;
 import br.com.copyimagem.mspersistence.core.usecases.interfaces.NaturalPersonCustomerService;
 import lombok.extern.log4j.Log4j2;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 
 @Log4j2
@@ -18,5 +23,10 @@ public class NaturalPersonCustomerController {
         this.naturalPersonCustomerService = naturalPersonCustomerService;
     }
 
+    @GetMapping( value = "/all" )
+    public ResponseEntity< List< NaturalPersonCustomerDTO > > getAllNaturalPersonCustomers() {
+
+        return ResponseEntity.ok().body( naturalPersonCustomerService.findAllNaturalPersonCustomer() );
+    }
 
 }
