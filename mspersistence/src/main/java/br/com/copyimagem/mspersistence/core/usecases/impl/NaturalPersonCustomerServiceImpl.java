142176df51369a2f7e303967a3dc6f98ae9a95fd
Claudio Carigé <ccarige@gmail.com>
2024-07-01 23:38:42 -0300

    [ addition ] : Created saveNaturalPersonCustomer  method

--- mspersistence/src/main/java/br/com/copyimagem/mspersistence/core/usecases/impl/NaturalPersonCustomerServiceImpl.java
+++ mspersistence/src/main/java/br/com/copyimagem/mspersistence/core/usecases/impl/NaturalPersonCustomerServiceImpl.java
@@ -1,8 +1,11 @@
 package br.com.copyimagem.mspersistence.core.usecases.impl;
 
+import br.com.copyimagem.mspersistence.core.domain.entities.Address;
+import br.com.copyimagem.mspersistence.core.domain.entities.CustomerContract;
 import br.com.copyimagem.mspersistence.core.domain.entities.NaturalPersonCustomer;
 import br.com.copyimagem.mspersistence.core.dtos.CustomerResponseDTO;
 import br.com.copyimagem.mspersistence.core.dtos.NaturalPersonCustomerDTO;
+import br.com.copyimagem.mspersistence.core.exceptions.DataIntegrityViolationException;
 import br.com.copyimagem.mspersistence.core.exceptions.NoSuchElementException;
 import br.com.copyimagem.mspersistence.core.usecases.interfaces.NaturalPersonCustomerService;
 import br.com.copyimagem.mspersistence.infra.persistence.repositories.AddressRepository;
@@ -11,6 +14,7 @@ import br.com.copyimagem.mspersistence.infra.persistence.repositories.CustomerRe
 import br.com.copyimagem.mspersistence.infra.persistence.repositories.NaturalPersonCustomerRepository;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
@@ -59,16 +63,54 @@ public class NaturalPersonCustomerServiceImpl implements NaturalPersonCustomerSe
         return convertObjectToObjectDTOService.convertToNaturalPersonCustomerDTO( naturalPersonCustomer );
     }
 
+    @Transactional
     @Override
-    public NaturalPersonCustomerDTO saveNaturalPersonCustomer( NaturalPersonCustomerDTO naturalPersonCustomer ) {
+    public NaturalPersonCustomerDTO saveNaturalPersonCustomer( NaturalPersonCustomerDTO naturalPersonCustomerDTO ) {
 
-        return null;
+        log.info( "[ INFO ] Saving customer. {}", naturalPersonCustomerDTO.getClass() );
+        naturalPersonCustomerDTO.setId( null );
+        Address address = addressRepository.save( naturalPersonCustomerDTO.getAddress() );
+        saveAddress( naturalPersonCustomerDTO );
+        generateCustomerContract( naturalPersonCustomerDTO );
+        existsCpfOrEmail( naturalPersonCustomerDTO );
+        NaturalPersonCustomer savedNaturalCustomer = naturalPersonCustomerRepository
+                .save( convertObjectToObjectDTOService.convertToNaturalPersonCustomer( naturalPersonCustomerDTO ) );
+        return convertObjectToObjectDTOService.convertToNaturalPersonCustomerDTO( savedNaturalCustomer );
     }
 
     @Override
     public CustomerResponseDTO findByCpf( String cpf ) {
 
         return null;
+    }
+
+    private void existsCpfOrEmail( NaturalPersonCustomerDTO naturalPersonCustomerDTO ) {
+
+        if( customerRepository.existsCustomerByPrimaryEmail( naturalPersonCustomerDTO.getPrimaryEmail() ) ) {
+            log.error( "[ ERROR ] Exception : Email already exists! : {}.", DataIntegrityViolationException.class );
+            throw new DataIntegrityViolationException( "Email already exists!" );
+        } else if( naturalPersonCustomerRepository.existsNaturalPersonCustomerByCpf( naturalPersonCustomerDTO.getCpf() ) ) {
+            log.error( "[ ERROR ] Exception : CPF already exists! : {}.", DataIntegrityViolationException.class );
+            throw new DataIntegrityViolationException( "CPF already exists!" );
+        }
+    }
+
+    private void generateCustomerContract( NaturalPersonCustomerDTO naturalPersonCustomerDTO ) {
+
+        if( naturalPersonCustomerDTO.getCustomerContract() == null ) {
+            CustomerContract contract = new CustomerContract();
+            naturalPersonCustomerDTO.setCustomerContract( contract );
+            customerContractRepository.save( contract );
+        }
+    }
+
+    private void saveAddress( NaturalPersonCustomerDTO naturalPersonCustomerDTO ) {
+
+        if( naturalPersonCustomerDTO.getAddress() != null ) {
+            naturalPersonCustomerDTO.setAddress( addressRepository.save( naturalPersonCustomerDTO.getAddress() ) );
+        } else {
+            throw new DataIntegrityViolationException( "Address is null!" );
+        }
     }
 
 }
