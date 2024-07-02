6e5b53f9b6b1188a42112b8f53014a24f6b602eb
Claudio Carigé <ccarige@gmail.com>
2024-07-01 22:55:17 -0300

    [ addition ] : Created findAllNaturalPersonCustomer method

--- mspersistence/src/main/java/br/com/copyimagem/mspersistence/core/usecases/impl/NaturalPersonCustomerServiceImpl.java
+++ mspersistence/src/main/java/br/com/copyimagem/mspersistence/core/usecases/impl/NaturalPersonCustomerServiceImpl.java
@@ -1,18 +1,52 @@
 package br.com.copyimagem.mspersistence.core.usecases.impl;
 
+import br.com.copyimagem.mspersistence.core.domain.entities.NaturalPersonCustomer;
 import br.com.copyimagem.mspersistence.core.dtos.CustomerResponseDTO;
 import br.com.copyimagem.mspersistence.core.dtos.NaturalPersonCustomerDTO;
 import br.com.copyimagem.mspersistence.core.usecases.interfaces.NaturalPersonCustomerService;
+import br.com.copyimagem.mspersistence.infra.persistence.repositories.AddressRepository;
+import br.com.copyimagem.mspersistence.infra.persistence.repositories.CustomerContractRepository;
+import br.com.copyimagem.mspersistence.infra.persistence.repositories.CustomerRepository;
+import br.com.copyimagem.mspersistence.infra.persistence.repositories.NaturalPersonCustomerRepository;
+import lombok.extern.log4j.Log4j2;
+import org.springframework.stereotype.Service;
 
 import java.util.List;
 
-
+@Log4j2
+@Service
 public class NaturalPersonCustomerServiceImpl implements NaturalPersonCustomerService {
+
+    private final NaturalPersonCustomerRepository naturalPersonCustomerRepository;
+
+    private final CustomerRepository customerRepository;
+
+    private final AddressRepository addressRepository;
+
+    private final CustomerContractRepository customerContractRepository;
+
+    private final ConvertObjectToObjectDTOService convertObjectToObjectDTOService;
+
+    public NaturalPersonCustomerServiceImpl( NaturalPersonCustomerRepository naturalPersonCustomerRepository,
+                                             CustomerRepository customerRepository,
+                                             AddressRepository addressRepository,
+                                             CustomerContractRepository customerContractRepository,
+                                             ConvertObjectToObjectDTOService convertObjectToObjectDTOService ) {
+
+        this.naturalPersonCustomerRepository = naturalPersonCustomerRepository;
+        this.customerRepository = customerRepository;
+        this.addressRepository = addressRepository;
+        this.customerContractRepository = customerContractRepository;
+        this.convertObjectToObjectDTOService = convertObjectToObjectDTOService;
+    }
 
     @Override
     public List< NaturalPersonCustomerDTO > findAllNaturalPersonCustomer() {
 
-        return List.of();
+        log.info( "[ INFO ] Finding all customers" );
+        List< NaturalPersonCustomer > custumerList = naturalPersonCustomerRepository.findAll();
+        return custumerList.stream()
+                                .map( convertObjectToObjectDTOService::convertToNaturalPersonCustomerDTO ).toList();
     }
 
     @Override
