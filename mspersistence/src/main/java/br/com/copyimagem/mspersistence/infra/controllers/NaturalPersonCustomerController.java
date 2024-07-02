aecb85902731697bcb38bbba05093336fb6acbf7
Claudio Carigé <ccarige@gmail.com>
2024-07-02 19:13:58 -0300

    [ addition ] : Created getNaturalPersonCustomerById method

--- mspersistence/src/main/java/br/com/copyimagem/mspersistence/infra/controllers/NaturalPersonCustomerController.java
+++ mspersistence/src/main/java/br/com/copyimagem/mspersistence/infra/controllers/NaturalPersonCustomerController.java
@@ -3,8 +3,10 @@ package br.com.copyimagem.mspersistence.infra.controllers;
 import br.com.copyimagem.mspersistence.core.dtos.NaturalPersonCustomerDTO;
 import br.com.copyimagem.mspersistence.core.usecases.interfaces.NaturalPersonCustomerService;
 import lombok.extern.log4j.Log4j2;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -27,6 +29,15 @@ public class NaturalPersonCustomerController {
     public ResponseEntity< List< NaturalPersonCustomerDTO > > getAllNaturalPersonCustomers() {
 
         return ResponseEntity.ok().body( naturalPersonCustomerService.findAllNaturalPersonCustomer() );
+    }
+
+    @GetMapping( value = "/{id}" )
+    public ResponseEntity< NaturalPersonCustomerDTO > getNaturalPersonCustomerById( @PathVariable Long id ) {
+
+        log.info( "[ INFO ] HTTP call with GET method with path: ../customers/pf/{id}" );
+        NaturalPersonCustomerDTO naturalPersonCustomerById =
+                naturalPersonCustomerService.findNaturalPersonCustomerById( id );
+        return ResponseEntity.ok().contentType( MediaType.APPLICATION_JSON ).body( naturalPersonCustomerById );
     }
 
 }
