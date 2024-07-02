ea8d0f0ab6af6a937778a9ac7f23adb5205a04ce
Claudio Carigé <ccarige@gmail.com>
2024-07-02 12:18:00 -0300

    [ addition ] : Created getLegalPersonalCustomerById method

--- mspersistence/src/main/java/br/com/copyimagem/mspersistence/infra/controllers/LegalPersonalCustomerController.java
+++ mspersistence/src/main/java/br/com/copyimagem/mspersistence/infra/controllers/LegalPersonalCustomerController.java
@@ -4,8 +4,10 @@ package br.com.copyimagem.mspersistence.infra.controllers;
 import br.com.copyimagem.mspersistence.core.dtos.LegalPersonalCustomerDTO;
 import br.com.copyimagem.mspersistence.core.usecases.interfaces.LegalPersonalCustomerService;
 import lombok.extern.log4j.Log4j2;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -28,6 +30,15 @@ public class LegalPersonalCustomerController {
     public ResponseEntity< List< LegalPersonalCustomerDTO > > getAllLegalPersonalCustomers() {
 
         return ResponseEntity.ok().body( legalPersonalCustomerService.findAllLegalPersonalCustomer() );
+    }
+
+    @GetMapping( value = "/{id}" )
+    public ResponseEntity< LegalPersonalCustomerDTO > getLegalPersonalCustomerById( @PathVariable Long id ) {
+
+        log.info( "[ INFO ] HTTP call with GET method with path: ../customers/pj/{id}" );
+        LegalPersonalCustomerDTO legalPersonalCustomerById = legalPersonalCustomerService
+                .findLegalPersonalCustomerById( id );
+        return ResponseEntity.ok().contentType( MediaType.APPLICATION_JSON ).body( legalPersonalCustomerById );
     }
 
 }
