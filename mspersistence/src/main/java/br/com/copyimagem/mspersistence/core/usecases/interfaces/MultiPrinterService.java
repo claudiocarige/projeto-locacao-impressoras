5702d5ff18a288d0645fefbb914068c65f44245b
Claudio Carigé <ccarige@gmail.com>
2024-07-04 21:51:20 -0300

    [ addition ] : Created MultiPrinterService interface

---
+++ mspersistence/src/main/java/br/com/copyimagem/mspersistence/core/usecases/interfaces/MultiPrinterService.java
@@ -0,0 +1,15 @@
+package br.com.copyimagem.mspersistence.core.usecases.interfaces;
+
+
+import br.com.copyimagem.mspersistence.core.dtos.MultiPrinterDTO;
+
+import java.util.List;
+
+
+public interface MultiPrinterService {
+
+    MultiPrinterDTO findMultiPrinterById( Integer id );
+
+    List< MultiPrinterDTO > findAllMultiPrinters();
+
+}
