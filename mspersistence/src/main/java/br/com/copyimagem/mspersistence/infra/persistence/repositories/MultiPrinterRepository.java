f8b541d730f492876fc10fb2e00f81bc529f73d3
Claudio Carigé <ccarige@gmail.com>
2024-07-04 22:56:10 -0300

    [ addition ] : Created findAllByCustomerId method in MultiPrinterRepository

--- mspersistence/src/main/java/br/com/copyimagem/mspersistence/infra/persistence/repositories/MultiPrinterRepository.java
+++ mspersistence/src/main/java/br/com/copyimagem/mspersistence/infra/persistence/repositories/MultiPrinterRepository.java
@@ -3,6 +3,10 @@ package br.com.copyimagem.mspersistence.infra.persistence.repositories;
 import br.com.copyimagem.mspersistence.core.domain.entities.MultiPrinter;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 
 public interface MultiPrinterRepository extends JpaRepository< MultiPrinter, Integer> {
+
+    List< MultiPrinter > findAllByCustomerId( Long customerId );
 }
