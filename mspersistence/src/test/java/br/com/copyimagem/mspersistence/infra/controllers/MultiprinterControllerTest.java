ebc1b7361955e31729b82669f41ff7bd6487ed87
Claudio Carigé <ccarige@gmail.com>
2024-07-06 14:42:35 -0300

    [ test ] : Created shouldReturnAListOfMultiPrinters test method

---
+++ mspersistence/src/test/java/br/com/copyimagem/mspersistence/infra/controllers/MultiprinterControllerTest.java
@@ -0,0 +1,73 @@
+package br.com.copyimagem.mspersistence.infra.controllers;
+
+import br.com.copyimagem.mspersistence.core.domain.entities.MultiPrinter;
+import br.com.copyimagem.mspersistence.core.dtos.MultiPrinterDTO;
+import br.com.copyimagem.mspersistence.core.usecases.interfaces.MultiPrinterService;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+
+import java.util.List;
+import java.util.Objects;
+
+import static br.com.copyimagem.mspersistence.core.domain.builders.MultiPrinterBuilder.oneMultiPrinter;
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+
+class MultiprinterControllerTest {
+
+    private MultiPrinter multiPrinter;
+
+    private MultiPrinterDTO multiPrinterDTO;
+
+    private MockMvc mockMvc;
+
+    @Mock
+    private MultiPrinterService multiPrinterService;
+
+    @InjectMocks
+    private MultiprinterController multiprinterController;
+
+    @BeforeEach
+    void setUp() {
+        MockitoAnnotations.openMocks( this );
+        mockMvc = MockMvcBuilders.standaloneSetup( multiprinterController ).build();
+        start();
+
+    }
+
+    @Test
+    @DisplayName( "Should return a list of MultiPrinter" )
+    void shouldReturnAListOfMultiPrinters() throws Exception {
+
+        when( multiPrinterService.findAllMultiPrinters() ).thenReturn( List.of( multiPrinterDTO ) );
+        ResponseEntity< List< MultiPrinterDTO > > multiPrinterDTOList = multiprinterController.findAllMultiPrinters();
+        assertNotNull( multiPrinterDTOList );
+        assertEquals( 1, Objects.requireNonNull( multiPrinterDTOList.getBody() ).size() );
+        assertEquals( multiPrinterDTO, multiPrinterDTOList.getBody().get( 0 ) );
+
+        mockMvc.perform( get( "/api/v1/multi-printer" ) )
+                .andExpect( status().isOk() )
+                .andExpect( content().contentType( MediaType.APPLICATION_JSON ) )
+                .andExpect(jsonPath("$[0].id").value(1))
+                .andExpect( jsonPath( "$[0].brand").value( "Epson" ) )
+                .andExpect( jsonPath( "$[0].serialNumber").value( "x1x2x3" ) );
+    }
+
+    private void start() {
+
+        multiPrinter = oneMultiPrinter().now();
+        multiPrinterDTO = oneMultiPrinter().nowDTO();
+    }
+
+}
