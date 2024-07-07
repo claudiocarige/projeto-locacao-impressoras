fbbb653291021f7850bb24832f9d10ac16702927
Claudio Carigé <ccarige@gmail.com>
2024-07-07 08:51:30 -0300

    [ addition ] : Created MonthlyPaymentDTO class

---
+++ mspersistence/src/main/java/br/com/copyimagem/mspersistence/core/dtos/MonthlyPaymentDTO.java
@@ -0,0 +1,61 @@
+package br.com.copyimagem.mspersistence.core.dtos;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+import lombok.*;
+
+import java.io.Serial;
+import java.io.Serializable;
+import java.time.LocalDate;
+
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
+public class MonthlyPaymentDTO implements Serializable {
+
+    @Serial
+    private static final long serialVersionUID = 1L;
+
+    private Long id;
+
+    private Integer monthPayment;
+
+    private Integer yearPayment;
+
+    private Integer quantityPrintsPB;
+
+    private Integer quantityPrintsColor;
+
+    private Integer printingFranchisePB;
+
+    private Integer printingFranchiseColor;
+
+    private String invoiceNumber;
+
+    private String ticketNumber;
+
+    private Double amountPrinter;
+
+    private Double monthlyAmount;
+
+    private Double excessValuePrintsPB;
+
+    private Double excessValuePrintsColor;
+
+    private Double rateExcessColorPrinting;
+
+    private Double rateExcessBlackAndWhitePrinting;
+
+    @JsonFormat( pattern = "dd-MM-yyyy" )
+    private LocalDate expirationDate;
+
+    @JsonFormat( pattern = "dd-MM-yyyy" )
+    private LocalDate paymentDate;
+
+    private String paymentStatus;
+
+    private Long customerId;
+
+}
