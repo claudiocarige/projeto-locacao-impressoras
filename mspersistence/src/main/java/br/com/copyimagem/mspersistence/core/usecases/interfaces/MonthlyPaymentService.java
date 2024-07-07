a1bf08489c55a499185d226c80df84b6f4058c26
Claudio Carigé <ccarige@gmail.com>
2024-07-07 15:17:54 -0300

    [ addition ] : Created MonthlyPaymentService interface class

---
+++ mspersistence/src/main/java/br/com/copyimagem/mspersistence/core/usecases/interfaces/MonthlyPaymentService.java
@@ -0,0 +1,10 @@
+package br.com.copyimagem.mspersistence.core.usecases.interfaces;
+
+import br.com.copyimagem.mspersistence.core.dtos.MonthlyPaymentDTO;
+import br.com.copyimagem.mspersistence.core.dtos.MonthlyPaymentRequest;
+
+
+public interface MonthlyPaymentService {
+    MonthlyPaymentDTO createMonthlyPayment( MonthlyPaymentRequest monthlyPaymentRequest );
+
+}
