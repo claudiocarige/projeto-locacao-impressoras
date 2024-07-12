e2b252238378bbbd50fbc74e0ce2af1155dd8a97
Claudio Carigé <ccarige@gmail.com>
2024-07-12 15:40:02 -0300

    [ addition ] : Created findMonthlyPaymentByAttributeAndValue persistence method

--- mspersistence/src/main/java/br/com/copyimagem/mspersistence/infra/persistence/repositories/MonthlyPaymentRepository.java
+++ mspersistence/src/main/java/br/com/copyimagem/mspersistence/infra/persistence/repositories/MonthlyPaymentRepository.java
@@ -3,6 +3,7 @@ package br.com.copyimagem.mspersistence.infra.persistence.repositories;
 import br.com.copyimagem.mspersistence.core.domain.entities.MonthlyPayment;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 
@@ -12,4 +13,9 @@ public interface MonthlyPaymentRepository extends JpaRepository< MonthlyPayment,
     @Query( "SELECT mp FROM MonthlyPayment mp WHERE mp.customer.id = :customerId" )
     List<MonthlyPayment> findAllMonthlyPaymentsByCustomerId( Long customerId );
 
+    @Query("SELECT mp FROM MonthlyPayment mp WHERE " +
+            "(:attribute = 'monthPayment' AND mp.monthPayment = :valueAttribute) OR " +
+            "(:attribute = 'yearPayment' AND mp.yearPayment = :valueAttribute) OR " +
+            "(:attribute = 'excessValuePrintsPB' AND mp.excessValuePrintsPB > :valueAttribute)")
+    List<MonthlyPayment> findMonthlyPaymentByAttributeAndValue(@Param("attribute") String attribute, @Param("valueAttribute") Object valueAttribute);
 }
