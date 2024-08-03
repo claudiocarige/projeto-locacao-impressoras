6f4c6f835a02119fa00248d1cf856517da2d9c53
Claudio Carigé <ccarige@gmail.com>
2024-08-03 15:07:35 -0300

    [ refactor ] : Inserted ActiveProfiles annotation

--- msmonthlypayment/src/test/java/br/com/copyimagem/msmonthlypayment/MsmonthlypaymentApplicationTests.java
+++ msmonthlypayment/src/test/java/br/com/copyimagem/msmonthlypayment/MsmonthlypaymentApplicationTests.java
@@ -2,8 +2,11 @@ package br.com.copyimagem.msmonthlypayment;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+
 
 @SpringBootTest
+@ActiveProfiles("test")
 class MsmonthlypaymentApplicationTests {
 
 	@Test
