3f3dafe13d9f93dc9a93e6f8b03b9262363b4827
Claudio Carigé <ccarige@gmail.com>
2024-08-04 09:31:45 -0300

    [ feat ] : Created a new technical support microservice - ms-help-Desk

---
+++ ms-help-desk/src/main/java/br/com/copyimagem/ms_help_desk/MsHelpDeskApplication.java
@@ -0,0 +1,13 @@
+package br.com.copyimagem.ms_help_desk;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsHelpDeskApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsHelpDeskApplication.class, args);
+	}
+
+}
---
+++ ms-help-desk/src/test/java/br/com/copyimagem/ms_help_desk/MsHelpDeskApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.copyimagem.ms_help_desk;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsHelpDeskApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
