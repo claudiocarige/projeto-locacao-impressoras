fbffa50123c241696c99a3fab04e3238222d112d
Claudio Carigé <ccarige@gmail.com>
2024-08-04 10:44:11 -0300

    [ addition ] : Created TicketDTO class

---
+++ ms-help-desk/src/main/java/br/com/copyimagem/ms_help_desk/core/domain/dtos/TicketDTO.java
@@ -0,0 +1,26 @@
+package br.com.copyimagem.ms_help_desk.core.domain.dtos;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class TicketDTO {
+
+    private Long id;
+    private String title;
+    private String description;
+    private String status;
+    private String priority;
+    private String type;
+    private String createdAt;
+    private String updatedAt;
+    private String closedAt;
+    private Long customer_id;
+    private Long technician_id;
+}
