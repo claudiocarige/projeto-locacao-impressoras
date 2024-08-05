ee8cc9aed0925b3c586843192660d1b23eb34581
Claudio Carigé <ccarige@gmail.com>
2024-08-04 21:44:04 -0300

    [ addition ] : Created new consult methods

--- ms-help-desk/src/main/java/br/com/copyimagem/ms_help_desk/infra/repositories/TicketRepository.java
+++ ms-help-desk/src/main/java/br/com/copyimagem/ms_help_desk/infra/repositories/TicketRepository.java
@@ -1,8 +1,18 @@
 package br.com.copyimagem.ms_help_desk.infra.repositories;
 
 import br.com.copyimagem.ms_help_desk.core.domain.entities.Ticket;
+import br.com.copyimagem.ms_help_desk.core.domain.enums.TicketStatus;
+import br.com.copyimagem.ms_help_desk.core.domain.enums.TicketType;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
 
-public interface TicketRepository extends JpaRepository< Ticket, Long> {
+
+public interface TicketRepository extends JpaRepository< Ticket, Long > {
+
+    List< Ticket > findByStatus( TicketStatus status );
+
+
+    List< Ticket> findByType( TicketType type );
+
 }
