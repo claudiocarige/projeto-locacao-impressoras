469dc52767b7b992b60c0da57b5b2d0d07439e60
Claudio Carigé <ccarige@gmail.com>
2024-08-04 21:56:30 -0300

    [ addition ] : Inserted EnableFeignClients annotation

--- ms-help-desk/src/main/java/br/com/copyimagem/ms_help_desk/MsHelpDeskApplication.java
+++ ms-help-desk/src/main/java/br/com/copyimagem/ms_help_desk/MsHelpDeskApplication.java
@@ -2,7 +2,10 @@ package br.com.copyimagem.ms_help_desk;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
+
+@EnableFeignClients
 @SpringBootApplication
 public class MsHelpDeskApplication {
 
