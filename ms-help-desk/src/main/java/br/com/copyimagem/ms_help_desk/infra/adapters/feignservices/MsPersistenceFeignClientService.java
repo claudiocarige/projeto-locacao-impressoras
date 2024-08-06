18b9f4361cd572dd90e1444d890684b07d2e0383
Claudio Carigé <ccarige@gmail.com>
2024-08-05 21:52:03 -0300

    [ refactor ] : Changed the return and inserted the full Path in MsPersistenceFeignClientService

--- ms-help-desk/src/main/java/br/com/copyimagem/ms_help_desk/infra/adapters/feignservices/MsPersistenceFeignClientService.java
+++ ms-help-desk/src/main/java/br/com/copyimagem/ms_help_desk/infra/adapters/feignservices/MsPersistenceFeignClientService.java
@@ -1,7 +1,7 @@
 package br.com.copyimagem.ms_help_desk.infra.adapters.feignservices;
 
 
-import br.com.copyimagem.ms_help_desk.core.domain.dtos.CustomerResponseDTO;
+import br.com.copyimagem.ms_help_desk.core.domain.dtos.UserRequestDTO;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -10,12 +10,12 @@ import org.springframework.web.bind.annotation.RequestParam;
 
 @FeignClient(
         value = "mspersistence",
-        path = "/api/v1"
+        path = "/api/v1/customers"
 )
 public interface MsPersistenceFeignClientService {
 
     @GetMapping( value = "/search-client" )
-    ResponseEntity< CustomerResponseDTO > searchCustomerByParams(
+    ResponseEntity< UserRequestDTO > searchCustomerByParams(
             @RequestParam( "typeParam" ) String typeParam,
             @RequestParam( "valueParam" ) String valueParam );
 }
