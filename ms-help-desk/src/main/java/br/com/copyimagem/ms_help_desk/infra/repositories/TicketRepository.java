731dafdbcf9f0e71e4482a851c564d746a8a40b6
Claudio Carigé <ccarige@gmail.com>
2024-08-12 23:02:49 -0300

    [ refactor ] : Removes specific update methods for simplification
    - Removed the 'updateTypeById', 'updateStatusById', and 'updatePriorityById' methods from the 'TicketRepository' repository.
    - Simplified the repository interface, leaving only the basic inheritance of 'JpaRepository<Ticket, Long>'.
    - The removal of these methods seeks to centralize the update logic in the corresponding service ('TicketServiceImpl'), avoiding logic duplication and facilitating future maintenance.
    - This change also promotes a better separation of responsibilities by keeping the repository focused on basic CRUD operations.

--- ms-help-desk/src/main/java/br/com/copyimagem/ms_help_desk/infra/repositories/TicketRepository.java
+++ ms-help-desk/src/main/java/br/com/copyimagem/ms_help_desk/infra/repositories/TicketRepository.java
@@ -1,41 +1,9 @@
 package br.com.copyimagem.ms_help_desk.infra.repositories;
 
 import br.com.copyimagem.ms_help_desk.core.domain.entities.Ticket;
-import br.com.copyimagem.ms_help_desk.core.domain.enums.TicketPriority;
-import br.com.copyimagem.ms_help_desk.core.domain.enums.TicketType;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Modifying;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
-import org.springframework.transaction.annotation.Transactional;
-
-import java.time.LocalDateTime;
 
 
 public interface TicketRepository extends JpaRepository< Ticket, Long > {
 
-    @Transactional
-    @Modifying
-    @Query( "UPDATE Ticket t SET t.type = :type, t.updatedAt = :updatedAt WHERE t.id = :id" )
-    int updateTypeById( @Param( "id" ) Long id,
-                          @Param( "type" ) TicketType type,
-                          @Param( "updatedAt" ) LocalDateTime updatedAt );
-
-    @Transactional
-    @Modifying
-    @Query(value = "UPDATE ticket SET status = :status, " +
-            "updated_at = CASE WHEN :dataField = 'updatedAt' THEN CURRENT_TIMESTAMP ELSE updated_at END, " +
-            "closed_at = CASE WHEN :dataField = 'closedAt' THEN CURRENT_TIMESTAMP ELSE closed_at END " +
-            "WHERE id = :id", nativeQuery = true)
-    int updateStatusById( @Param( "id" ) Long id,
-                        @Param( "status" ) String status,
-                        @Param( "dataField") String dataField);
-
-
-    @Transactional
-    @Modifying
-    @Query( "UPDATE Ticket t SET t.priority = :priority, t.updatedAt = :updatedAt WHERE t.id = :id" )
-    int updatePriorityById( @Param( "id" ) Long id,
-                          @Param( "priority" ) TicketPriority priority,
-                          @Param( "updatedAt" ) LocalDateTime updatedAt );
 }
