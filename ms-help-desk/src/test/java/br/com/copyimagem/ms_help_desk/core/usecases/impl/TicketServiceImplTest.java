7853da770aec9ef845bb8c6841d9b3694064cc37
Claudio Carigé <ccarige@gmail.com>
2024-08-16 22:52:11 -0300

    [ refactor ] : Code reformatting to improve readability and structure

--- ms-help-desk/src/test/java/br/com/copyimagem/ms_help_desk/core/usecases/impl/TicketServiceImplTest.java
+++ ms-help-desk/src/test/java/br/com/copyimagem/ms_help_desk/core/usecases/impl/TicketServiceImplTest.java
@@ -3,6 +3,9 @@ package br.com.copyimagem.ms_help_desk.core.usecases.impl;
 import br.com.copyimagem.ms_help_desk.core.domain.dtos.TicketDTO;
 import br.com.copyimagem.ms_help_desk.core.domain.dtos.UserRequestDTO;
 import br.com.copyimagem.ms_help_desk.core.domain.entities.Ticket;
+import br.com.copyimagem.ms_help_desk.core.domain.enums.TicketPriority;
+import br.com.copyimagem.ms_help_desk.core.domain.enums.TicketStatus;
+import br.com.copyimagem.ms_help_desk.core.domain.enums.TicketType;
 import br.com.copyimagem.ms_help_desk.core.exceptions.CustomFeignException;
 import br.com.copyimagem.ms_help_desk.core.exceptions.IllegalArgumentException;
 import br.com.copyimagem.ms_help_desk.core.exceptions.NoSuchElementException;
@@ -25,7 +28,7 @@ import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.Mockito.*;
 
 
-@ExtendWith( MockitoExtension.class)
+@ExtendWith( MockitoExtension.class )
 class TicketServiceImplTest {
 
 
@@ -47,105 +50,118 @@ class TicketServiceImplTest {
 
     @BeforeEach
     void setUp() {
+
         startEntities();
     }
 
 
     @Test
-    @DisplayName("Should return a ticketDTO with success")
+    @DisplayName( "Should return a ticketDTO with success" )
     void shouldReturnATicketDTOWithSuccess() {
-        when(ticketRepository.findById(1L)).thenReturn( Optional.of(ticket));
-        when(convertEntityAndDTOService.convertEntityToDTO(ticket)).thenReturn(ticketDTO);
-        TicketDTO result = ticketService.getTicketById(1L);
-        assertEquals( ticketDTO, result);
-        assertEquals( ticketDTO.getId(), result.getId());
+
+        when( ticketRepository.findById( 1L ) ).thenReturn( Optional.of( ticket ) );
+        when( convertEntityAndDTOService.convertEntityToDTO( ticket ) ).thenReturn( ticketDTO );
+        TicketDTO result = ticketService.getTicketById( 1L );
+        assertEquals( ticketDTO, result );
+        assertEquals( ticketDTO.getId(), result.getId() );
     }
 
     @Test
-    @DisplayName("Should return a exception when ticket not found")
+    @DisplayName( "Should return a exception when ticket not found" )
     void shouldReturnAExceptionWhenTicketNotFound() {
-        when(ticketRepository.findById(1L)).thenReturn( Optional.empty());
+
+        when( ticketRepository.findById( 1L ) ).thenReturn( Optional.empty() );
         try {
-            ticketService.getTicketById(1L);
-        } catch ( NoSuchElementException e) {
-            assertEquals("Ticket not found", e.getMessage());
+            ticketService.getTicketById( 1L );
+        } catch( NoSuchElementException e ) {
+            assertEquals( "Ticket not found", e.getMessage() );
         }
     }
 
     @Test
-    @DisplayName("Should return a list of ticketDTO.")
+    @DisplayName( "Should return a list of ticketDTO." )
     void shouldReturnAListOfTicketDTO() {
-        when(ticketRepository.findAll()).thenReturn( List.of(ticket));
-        when(convertEntityAndDTOService.convertEntityListToDTOList(List.of(ticket))).thenReturn(List.of(ticketDTO));
-        List<TicketDTO> result = ticketService.getAllTickets();
-        assertEquals( List.of(ticketDTO), result);
-        assertEquals( ticketDTO.getId(), result.get(0).getId());
+
+        when( ticketRepository.findAll() ).thenReturn( List.of( ticket ) );
+        when( convertEntityAndDTOService
+                                .convertEntityListToDTOList( List.of( ticket ) ) ).thenReturn( List.of( ticketDTO ) );
+        List< TicketDTO > result = ticketService.getAllTickets();
+        assertEquals( List.of( ticketDTO ), result );
+        assertEquals( ticketDTO.getId(), result.get( 0 ).getId() );
     }
 
     @Test
-    @DisplayName( "Should create a ticket with success")
+    @DisplayName( "Should create a ticket with success" )
     void shouldCreateATicketWithSuccess() {
-        ResponseEntity<UserRequestDTO> clientResponse = ResponseEntity.ok(new UserRequestDTO(1L, "Client Name"));
-        ResponseEntity<UserRequestDTO> technicalResponse = ResponseEntity.ok(new UserRequestDTO(2L, "Technical Name"));
 
-        when(msPersistenceFeignClientService.searchCustomerByParams("clientname", ticketDTO.getClientName())).thenReturn(clientResponse);
-        when(msPersistenceFeignClientService.searchCustomerByParams("clientname", ticketDTO.getTechnicalName())).thenReturn(technicalResponse);
-        when(convertEntityAndDTOService.convertDTOToEntity(ticketDTO)).thenReturn(ticket);
-        when(ticketRepository.save(ticket)).thenReturn(ticket);
-        when(convertEntityAndDTOService.convertEntityToDTO(ticket)).thenReturn(ticketDTO);
-        TicketDTO result = ticketService.createTicket(ticketDTO);
+        ResponseEntity< UserRequestDTO > clientResponse = ResponseEntity.ok( new UserRequestDTO( 1L, "Client Name" ) );
+        ResponseEntity< UserRequestDTO > technicalResponse =
+                                                        ResponseEntity.ok( new UserRequestDTO( 2L, "Technical Name" ) );
 
-        assertNotNull(result);
-        assertEquals(1L, result.getId());
-        assertEquals("Client Name", result.getClientName());
+        when( msPersistenceFeignClientService
+                      .searchCustomerByParams( "clientname", ticketDTO.getClientName() ) ).thenReturn( clientResponse );
+        when( msPersistenceFeignClientService
+                .searchCustomerByParams( "clientname", ticketDTO.getTechnicalName() ) ).thenReturn( technicalResponse );
+        when( convertEntityAndDTOService.convertDTOToEntity( ticketDTO ) ).thenReturn( ticket );
+        when( ticketRepository.save( ticket ) ).thenReturn( ticket );
+        when( convertEntityAndDTOService.convertEntityToDTO( ticket ) ).thenReturn( ticketDTO );
+        TicketDTO result = ticketService.createTicket( ticketDTO );
 
-        verify(msPersistenceFeignClientService, times(1)).searchCustomerByParams("clientname", "Client Name");
-        verify(msPersistenceFeignClientService, times(1)).searchCustomerByParams("clientname", "Technical Name");
+        assertNotNull( result );
+        assertEquals( 1L, result.getId() );
+        assertEquals( "Client Name", result.getClientName() );
+
+        verify( msPersistenceFeignClientService, times( 1 ) ).searchCustomerByParams( "clientname", "Client Name" );
+        verify( msPersistenceFeignClientService, times( 1 ) ).searchCustomerByParams( "clientname", "Technical Name" );
 
     }
 
     @Test
-    @DisplayName("Should return a exception when client not found")
+    @DisplayName( "Should return a exception when client not found" )
     void shouldReturnAExceptionWhenClientNotFound() {
-        ResponseEntity<UserRequestDTO> clientResponse = ResponseEntity.ok(new UserRequestDTO(1L, "Client Name"));
-        ResponseEntity<UserRequestDTO> technicalResponse = ResponseEntity.ok(new UserRequestDTO(2L, "Technical Name"));
 
-        when(msPersistenceFeignClientService.searchCustomerByParams("clientname", ticketDTO.getClientName())).thenReturn(clientResponse);
-        when(msPersistenceFeignClientService.searchCustomerByParams("clientname", ticketDTO.getTechnicalName())).thenReturn(technicalResponse);
-        when(convertEntityAndDTOService.convertDTOToEntity(ticketDTO)).thenReturn(ticket);
-        when(ticketRepository.save(ticket)).thenReturn(ticket);
-        when(convertEntityAndDTOService.convertEntityToDTO(ticket)).thenReturn(ticketDTO);
+        ResponseEntity< UserRequestDTO > clientResponse = ResponseEntity.ok( new UserRequestDTO( 1L, "Client Name" ) );
+        ResponseEntity< UserRequestDTO > technicalResponse = ResponseEntity.
+                                                                      ok( new UserRequestDTO( 2L, "Technical Name" ) );
+
+        when( msPersistenceFeignClientService
+                .searchCustomerByParams( "clientname", ticketDTO.getClientName() ) ).thenReturn( clientResponse );
+        when( msPersistenceFeignClientService
+                .searchCustomerByParams( "clientname", ticketDTO.getTechnicalName() ) ).thenReturn( technicalResponse );
+        when( convertEntityAndDTOService.convertDTOToEntity( ticketDTO ) ).thenReturn( ticket );
+        when( ticketRepository.save( ticket ) ).thenReturn( ticket );
+        when( convertEntityAndDTOService.convertEntityToDTO( ticket ) ).thenReturn( ticketDTO );
 
         try {
-            ticketService.createTicket(ticketDTO);
-        } catch ( CustomFeignException e) {
-            assertEquals("Client not found", e.getMessage());
-            System.out.println(e.getMessage());
+            ticketService.createTicket( ticketDTO );
+        } catch( CustomFeignException e ) {
+            assertEquals( "Client not found", e.getMessage() );
+            System.out.println( e.getMessage() );
         }
     }
 
     @Test
-    @DisplayName("Should delete a ticket with success")
+    @DisplayName( "Should delete a ticket with success" )
     void shouldDeleteATicketWithSucess() {
-        ticketDTO.setStatus("ERROR");
-        when(ticketRepository.findById(1L)).thenReturn( Optional.of(ticket));
-        when(ticketService.getTicketById(1L)).thenReturn(ticketDTO);
-        ticketService.deleteTicket(1L);
-        verify(ticketRepository, times(1)).deleteById(1L);
+
+        ticketDTO.setStatus( "ERROR" );
+        when( ticketRepository.findById( 1L ) ).thenReturn( Optional.of( ticket ) );
+        when( ticketService.getTicketById( 1L ) ).thenReturn( ticketDTO );
+        ticketService.deleteTicket( 1L );
+        verify( ticketRepository, times( 1 ) ).deleteById( 1L );
 
     }
 
     @Test
-    @DisplayName("Should return a exception when ticket is not in error status")
+    @DisplayName( "Should return a exception when ticket is not in error status" )
     void shouldReturnAExceptionWhenTicketIsNotInErrorStatus() {
-        ticketDTO.setStatus("OPEN");
-        when(ticketRepository.findById(1L)).thenReturn( Optional.of(ticket));
-        when(ticketService.getTicketById(1L)).thenReturn(ticketDTO);
-        IllegalArgumentException exception = assertThrows( IllegalArgumentException.class, () -> {
 
-            ticketService.deleteTicket(1L);
-        });
-        assertEquals("Unable to delete the Ticket", exception.getMessage());
+        ticketDTO.setStatus( "OPEN" );
+        when( ticketRepository.findById( 1L ) ).thenReturn( Optional.of( ticket ) );
+        when( ticketService.getTicketById( 1L ) ).thenReturn( ticketDTO );
+        IllegalArgumentException exception = assertThrows(
+                                          IllegalArgumentException.class, () -> { ticketService.deleteTicket( 1L ); } );
+        assertEquals( "Unable to delete the Ticket", exception.getMessage() );
     }
 
     @Test
@@ -155,8 +171,8 @@ class TicketServiceImplTest {
         when( ticketRepository.findById( 1L ) ).thenReturn( Optional.of( ticket ) );
         ticketService.updateTicketsByStatus( 1L, TicketStatus.IN_PROGRESS );
 
-        assert ( ticket.getStatus().equals( TicketStatus.IN_PROGRESS ) );
-        assert ( ticket.getUpdatedAt() != null );
+        assertEquals( ticket.getStatus(), TicketStatus.IN_PROGRESS );
+        assertNotNull( ticket.getUpdatedAt() );
         verify( ticketRepository, times( 1 ) ).save( ticket );
     }
 
@@ -168,9 +184,7 @@ class TicketServiceImplTest {
         when( ticketRepository.findById( 1L ) ).thenReturn( Optional.of( ticket ) );
 
         IllegalArgumentException exception = assertThrows(
-                IllegalArgumentException.class,
-                () -> ticketService.updateTicketsByStatus( 1L, TicketStatus.CLOSED )
-        );
+                IllegalArgumentException.class, () -> ticketService.updateTicketsByStatus( 1L, TicketStatus.CLOSED ) );
         assertEquals( "Ticket is already closed", exception.getMessage() );
         verify( ticketRepository, never() ).save( any( Ticket.class ) );
     }
@@ -178,11 +192,13 @@ class TicketServiceImplTest {
     @Test
     @DisplayName( "Should Update a Ticket Type" )
     void shouldUpdateATicketsType() {
+
         when( ticketRepository.findById( 1L ) ).thenReturn( Optional.of( ticket ) );
         ticketService.updateTicketsByType( 1L, TicketType.REFILL );
 
-        assertEquals ( ticket.getType(), TicketType.REFILL  );
+        assertEquals( ticket.getType(), TicketType.REFILL );
         assertNotNull( ticket.getUpdatedAt() );
+        verify( ticketRepository, times( 1 ) ).save( ticket );
 
     }
 
@@ -193,23 +209,25 @@ class TicketServiceImplTest {
         when( ticketRepository.findById( 1L ) ).thenReturn( Optional.of( ticket ) );
         ticketService.updateTicketsByPriority( 1L, TicketPriority.HIGH );
 
-        assertEquals ( ticket.getPriority(), TicketPriority.HIGH  );
+        assertEquals( ticket.getPriority(), TicketPriority.HIGH );
         assertNotNull( ticket.getUpdatedAt() );
         verify( ticketRepository, times( 1 ) ).save( ticket );
     }
 
-    void startEntities(){
+    void startEntities() {
+
         ticket = new Ticket();
-        ticket.setId(1L);
+        ticket.setId( 1L );
+        ticket.setStatus( TicketStatus.OPEN );
         ticketDTO = new TicketDTO();
-        ticketDTO.setId(1L);
-        ticketDTO.setClientName("Client Name");
-        ticketDTO.setTechnicalName("Technical Name");
-        ticketDTO.setPriority("HIGH");
-        ticketDTO.setStatus("OPEN");
-        ticketDTO.setType("INCIDENT");
-        ticketDTO.setCreatedAt( LocalDateTime.now());
-        ticketDTO.setUpdatedAt(LocalDateTime.now());
+        ticketDTO.setId( 1L );
+        ticketDTO.setClientName( "Client Name" );
+        ticketDTO.setTechnicalName( "Technical Name" );
+        ticketDTO.setPriority( "HIGH" );
+        ticketDTO.setStatus( "OPEN" );
+        ticketDTO.setType( "INCIDENT" );
+        ticketDTO.setCreatedAt( LocalDateTime.now() );
+        ticketDTO.setUpdatedAt( LocalDateTime.now() );
     }
 
 }
