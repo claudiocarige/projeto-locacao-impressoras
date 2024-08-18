4e13068d4deebcf784e6e473ff3364fc5ad55c04
Claudio Carigé <ccarige@gmail.com>
2024-08-18 01:21:58 -0300

    [ feat ] : Created a new service, ms-user-service

--- ms-help-desk/src/test/java/br/com/copyimagem/ms_help_desk/MsHelpDeskApplicationTests.java
+++ ms-help-desk/src/test/java/br/com/copyimagem/ms_help_desk/MsHelpDeskApplicationTests.java
@@ -2,8 +2,11 @@ package br.com.copyimagem.ms_help_desk;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+
 
 @SpringBootTest
+@ActiveProfiles("test")
 class MsHelpDeskApplicationTests {
 
 	@Test
---
+++ ms-user-service/src/test/java/br/com/copyimagem/ms_user_service/MsUserServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.copyimagem.ms_user_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsUserServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
