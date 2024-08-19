4958508f338992fc8b689fb3471d30506cc923bf
Claudio Carigé <ccarige@gmail.com>
2024-08-19 20:57:13 -0300

    [ addition ] : Inserted the dependence on the H2 bank and ModelMapper
    - Created Bean for ModelMapper
    - Done the configuration in the application-test.yml file

---
+++ ms-user-service/src/main/java/br/com/copyimagem/ms_user_service/infra/config/ModelMapperConfig.java
@@ -0,0 +1,17 @@
+package br.com.copyimagem.ms_user_service.infra.config;
+
+import org.modelmapper.ModelMapper;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+
+@Configuration
+public class ModelMapperConfig {
+
+    @Bean
+    public ModelMapper modelMapper() {
+
+        return new ModelMapper();
+    }
+
+}
