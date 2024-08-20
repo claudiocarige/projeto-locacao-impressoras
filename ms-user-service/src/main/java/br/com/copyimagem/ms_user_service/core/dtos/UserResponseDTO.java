d2436e71dd539f57f47b1f1996bfb0114a1315c8
Claudio Carigé <ccarige@gmail.com>
2024-08-19 23:05:38 -0300

    [ addition ] : Created UserResponseDTO class

--- ms-user-service/src/main/java/br/com/copyimagem/ms_user_service/core/dtos/UserResponseDTO.java
+++ ms-user-service/src/main/java/br/com/copyimagem/ms_user_service/core/dtos/UserResponseDTO.java
@@ -7,14 +7,14 @@ import java.io.Serializable;
 import java.util.Set;
 
 
-public record UserResponseDTO(  Long id,
-                                String name,
-                                String email,
-                                String lastPasswordReset,
-                                String codeAuthenticator,
-                                Integer loginAttempts,
-                                Boolean locked,
-                                Set< ProfileEnum > profiles
+public record UserResponseDTO( Long id,
+                               String name,
+                               String email,
+                               String lastPasswordReset,
+                               String codeAuthenticator,
+                               Integer loginAttempts,
+                               Boolean locked,
+                               Set< ProfileEnum > profiles
 
 ) implements Serializable {
 
