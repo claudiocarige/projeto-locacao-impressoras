fcdc63f8b3143913d14f704fa489ef6053a5638f
Claudio Carigé <ccarige@gmail.com>
2024-08-19 23:09:54 -0300

    [ test ] : Created the test UserServiceImplTest class and implemented shouldSaveANewUser method

---
+++ ms-user-service/src/test/java/br/com/copyimagem/ms_user_service/core/usecases/impl/UserServiceImplTest.java
@@ -0,0 +1,91 @@
+package br.com.copyimagem.ms_user_service.core.usecases.impl;
+
+import br.com.copyimagem.ms_user_service.core.domain.entities.User;
+import br.com.copyimagem.ms_user_service.core.domain.enums.ProfileEnum;
+import br.com.copyimagem.ms_user_service.core.dtos.UserCreateResponse;
+import br.com.copyimagem.ms_user_service.core.dtos.UserRequestDTO;
+import br.com.copyimagem.ms_user_service.infra.persistence.UserRepository;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.modelmapper.ModelMapper;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+import java.util.Set;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.*;
+
+
+@ExtendWith( MockitoExtension.class )
+class UserServiceImplTest {
+
+    private User user;
+
+    private UserRequestDTO userRequestDTO;
+
+    private UserCreateResponse userCreateResponse;
+
+    @Mock
+    private UserRepository userRepository;
+
+    @Mock
+    private ModelMapper modelMapper;
+
+    @Mock
+    private BCryptPasswordEncoder bcryptPasswordEncoder;
+
+    @InjectMocks
+    private UserServiceImpl userServiceImpl;
+
+
+    @BeforeEach
+    void setUp() {
+
+        startEntity();
+    }
+
+    @Test
+    @DisplayName( "Should save a new User" )
+    void shouldSaveANewUser() {
+
+        when( userRepository.save( user ) ).thenReturn( user );
+        when( userRepository.findByEmail( "ccarige@gmail.com" ) ).thenReturn( null );
+        when( modelMapper.map( userRequestDTO, User.class ) ).thenReturn( user );
+        when( modelMapper.map( user, UserCreateResponse.class ) ).thenReturn( userCreateResponse );
+        when( bcryptPasswordEncoder.encode( userRequestDTO.getPassword() ) ).thenReturn( "1A2B3C4D5E6F-encoded" );
+
+        UserCreateResponse result = userServiceImpl.save( userRequestDTO );
+
+        assertEquals( userCreateResponse, result );
+        assertEquals( userCreateResponse.email(), result.email() );
+        assertEquals( userCreateResponse.profiles(), result.profiles() );
+
+        verify( userRepository, times( 1 ) ).save( user );
+        verify( userRepository, times( 1 ) ).findByEmail( "ccarige@gmail.com" );
+    }
+
+    private void startEntity() {
+
+        user = new User();
+        user.setId( 1L );
+        user.setName( "Claudio Carigé" );
+        user.setEmail( "ccarige@gmail.com" );
+        user.setProfiles( Set.of( ProfileEnum.ROLE_CUSTOMER ) );
+        user.setPassword( "1A2B3C4D5E6F-encoded" );
+        userRequestDTO = new UserRequestDTO( "Claudio Carigé",
+                "ccarige@gmail.com",
+                "1A2B3C4D5E6F",
+                Set.of( ProfileEnum.ROLE_CUSTOMER ) );
+        userCreateResponse = new UserCreateResponse( 1L,
+                "Cláudio Carigé",
+                "ccarige@gmail.com",
+                Set.of( ProfileEnum.ROLE_CUSTOMER ) );
+
+    }
+
+}
