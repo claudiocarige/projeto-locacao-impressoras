c7e91c28ec0a062efa0d300490338000eb04ca9c
Claudio Carigé <ccarige@gmail.com>
2024-09-09 23:38:48 -0300

    [ refactor ] : UserCreateReponse class changed to a traditional class

--- ms-user-service/src/main/java/br/com/copyimagem/ms_user_service/core/dtos/UserCreateResponse.java
+++ ms-user-service/src/main/java/br/com/copyimagem/ms_user_service/core/dtos/UserCreateResponse.java
@@ -1,18 +1,26 @@
 package br.com.copyimagem.ms_user_service.core.dtos;
 
 import br.com.copyimagem.ms_user_service.core.domain.enums.ProfileEnum;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 import java.io.Serial;
 import java.io.Serializable;
 import java.util.Set;
 
-
-public record UserCreateResponse ( Long id,
-                                   String name,
-                                   String email,
-                                   Set< ProfileEnum > profiles
-) implements Serializable {
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class UserCreateResponse implements Serializable {
 
     @Serial
     private static final long serialVersionUID = 1L;
+
+    private Long id;
+    private String name;
+    private String email;
+    private Set< ProfileEnum > profiles;
 }
